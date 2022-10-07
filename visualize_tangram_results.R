616a78cdd5956aba0d6fba8430b845221ea69a00
Judith Bernett <bernett@in.tum.de>
2022-10-07 13:24:24 +0200

    code for visualizing the results

--- visualize_tangram_results.R
+++ visualize_tangram_results.R
@@ -5,28 +5,15 @@ library(ggplot2)
 # Layout of the cells distributed over 10 runs #
 ################################################
 
-mapping_df <- fread("~/PycharmProjects/SpatialTranscriptomics/dfs/mapping_df2.csv")
-#mapping_df <- fread("~/PycharmProjects/SpatialTranscriptomics/dfs/mapping_df.csv")
-mapping_df$run <- as.factor(mapping_df$run)
-mapping_df$probability[mapping_df$probability < 0.0001] <- NA
-#mapping_df <- mapping_df[complete.cases(mapping_df)]
+mapping_SlideSeq <- fread("data/mapping_df_SlideSeq.csv")
+mapping_STARmap <- fread("data/mapping_df_STARmap.csv")
+mapping_SlideSeq$run <- as.factor(mapping_SlideSeq$run)
+mapping_STARmap$run <- as.factor(mapping_STARmap$run)
+mapping_SlideSeq$probability[mapping_SlideSeq$probability < 0.0001] <- NA
 
-ggplot(mapping_df, aes(x=probability, fill = run))+
-  geom_histogram()+
-  scale_y_log10()+
-  facet_wrap(~run)+
-  theme_minimal()
+dir.create("visualizations")
 
-q <- mapping_df[, quantile(probability, na.rm=TRUE), by = run]
-q$quantile <- rep(c(0,25,50,75,100), 10)
-q$quantile <- as.factor(q$quantile)
-ggplot(q, aes(x = quantile, y = V1))+
-  geom_boxplot()+
-  theme_minimal()+
-  scale_y_log10()+
-  labs(y = "probability")
-
-g <- ggplot(mapping_df[cell %in% c(245,1146,8186,14441,15867)], aes(x=x, y=y))+
+g <- ggplot(mapping_SlideSeq[cell %in% c(245,1146,8186,14441,15867)], aes(x=x, y=y))+
   geom_point(aes(color=probability, alpha = probability), size=0.8)+
   facet_grid(cell ~ run)+
   #theme_minimal()+
@@ -36,10 +23,10 @@ g <- ggplot(mapping_df[cell %in% c(245,1146,8186,14441,15867)], aes(x=x, y=y))+
         rect = element_blank())+
   labs(x = "Run", y = "Cell ID")
 g
-ggsave("~/Downloads/mapping_slideseq.pdf", plot=g)
+ggsave("visualizations/mapping_slideseq.pdf", plot=g)
 
-mapping_high <- mapping_df[probability > 0.5]
-background <- mapping_df[run == 0, ]
+mapping_high <- mapping_STARmap[probability > 0.5]
+background <- mapping_STARmap[run == 0, ]
 background$probability <- 0
 background$run <- as.numeric(background$run)
 background$run <- -1
@@ -57,44 +44,43 @@ cols <- c("background" = "#cfcfcf",
          "7" = "#CC79A7",
          "8" = "#CC6666",
          "9" = "#9999CC")
-ggplot(mapping_high[cell %in% c(122, 239, 753, 1375, 1457, 1914, 2433, 2999, 7594, 7933, 9116, 10622, 10932, 11061, 12923, 13506)], aes(x=x, y=y, color = run, alpha = probability))+
+set.seed(1234)
+random_cell <- sort(sample(unique(mapping_high$cell), 16))
+ggplot(mapping_high[cell %in% random_cell], aes(x=x, y=y, color = run, alpha = probability))+
   geom_point()+
   facet_wrap(~ cell, ncol = 4)+
   theme_void()+
   scale_color_manual(values = cols)+
   theme(text = element_text(size = 20))
-ggsave("~/Downloads/mapping_starmap.pdf")
+ggsave("visualizations/mapping_starmap.pdf", width=8, height=6)
 
 ################################################
 # Correlation plots                            #
 ################################################
 
-cor_sm <- fread("~/Downloads/ST/correlations_all_starmap.csv")
+cor_sm <- fread("data/correlations_mappings_STARmap.csv")
 colnames(cor_sm) <- c("V1", "Correlation")
-#cor_sm$Subset <- "Training Genes"
 cor_sm$dataset <- "STARmap"
 cor_sm$matrix <- "Mapping"
 cor_sm <- cor_sm[, c(1,4,2,3)]
 
-cor_slideseq <- fread("~/Downloads/ST/correlations_all_slideseq.csv")
+cor_slideseq <- fread("data/correlations_mappings_SlideSeq.csv")
 colnames(cor_slideseq) <- c("V1", "Correlation")
-#cor_slideseq$Subset <- "Training Genes"
 cor_slideseq$dataset <- "SlideSeq"
 cor_slideseq$matrix <- "Mapping"
 cor_slideseq <- cor_slideseq[, c(1,4,2,3)]
 
-
-expr_sm <- fread("~/Downloads/ST/correlations_expr_starmap.csv")
+expr_sm <- fread("data/correlations_expr_STARmap.csv")
 colnames(expr_sm) <- c("V1", "Expression", "Expression")
 expr_sm <- melt(expr_sm[, -1], variable.name = "matrix", value.name = "Correlation")
 expr_sm$dataset <- "STARmap"
-#expr_sm$matrix <- "Expression"
+expr_sm$matrix <- "Expression"
 
-expr_slideseq <- fread("~/Downloads/ST/correlations_expr_slideseq.csv")
+expr_slideseq <- fread("data/correlations_expr_SlideSeq.csv")
 colnames(expr_slideseq) <- c("V1", "Expression", "Expression")
 expr_slideseq <- melt(expr_slideseq[, -1], variable.name = "matrix", value.name = "Correlation")
 expr_slideseq$dataset <- "SlideSeq"
-#expr_slideseq$matrix <- "Expression"
+expr_slideseq$matrix <- "Expression"
 
 cors <- rbindlist(list(cor_sm[, -1], cor_slideseq[, -1], expr_sm, expr_slideseq))
 
@@ -102,26 +88,6 @@ ggplot(cors, aes(x = matrix, y = Correlation, color = dataset)) +
   geom_boxplot() +
   theme_minimal() +
   labs(x = "Matrix", y = "Pairwise Correlation")+
-  theme(text = element_text(size = 15))
-ggsave("~/Downloads/correlations.pdf", height = 3, width = 8)
-
-
-
-probabilities <- fread("~/PycharmProjects/SpatialTranscriptomics/probabilities0_1.csv")
-q0 <- quantile(probabilities$run_0)
-q1 <- quantile(probabilities$run_1)
-q0 <- data.table(quantile = names(q0), probabilities = q0, run = 0)
-q1 <- data.table(quantile = names(q1), probabilities = q1, run = 1)
-q <- rbind(q0, q1)
-q$quantile <- factor(q$quantile, levels = c("0%", "25%", "50%", "75%", "100%"))
-ggplot(q, aes(x = quantile, y = probabilities))+
-  geom_boxplot()+
-  theme_minimal()+
-  scale_y_log10()+
-  labs(y = "probability")
-
-
-ggplot(probabilities[sample(nrow(probabilities), nrow(probabilities) * 0.3)], aes(x=run_0))+
-  geom_histogram()+
-  scale_y_log10()+
-  theme_minimal()
+  theme(text = element_text(size = 15))+
+  ylim(0,1)
+ggsave("visualizations/correlations.pdf", height = 3, width = 8)
